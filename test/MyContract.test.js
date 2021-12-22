03f0a317b9f43af5682b5bdf4d7a538a7104f1b7
Max Radin <radin.max@gmail.com>
2021-12-22 14:32:22 -0800

    Updating tests

--- test/MyContract.test.js
+++ test/MyContract.test.js
@@ -1,18 +1,29 @@
 const { expect } = require("chai");
 
+const product = 15;
+claim = "hello";
+
 describe("MyContract", () => {
-  it("should return its name", async () => {
+  it("should have the specified product", async () => {
     const MyContract = await ethers.getContractFactory("MyContract");
-    const myContract = await MyContract.deploy("My Contract");
+    const myContract = await MyContract.deploy(product);
 
     await myContract.deployed();
-    expect(await myContract.getName()).to.equal("My Contract");
+    expect(await myContract.product()).to.equal(product);
   });
-  it("should change its name when requested", async () => {
+  it("should keep track of submitted claims", async () => {
     const MyContract = await ethers.getContractFactory("MyContract");
-    const myContract = await MyContract.deploy("My Contract");
+    const myContract = await MyContract.deploy(product);
 
-    await myContract.changeName("Another Contract");
-    expect(await myContract.getName()).to.equal("Another Contract");
+    await myContract.deployed();
+    await myContract.submitClaim(claim);
+    expect(await myContract.claims(claim)).to.be.true;
+  });
+  it("should allow withdrawl of funds", async () => {
+    const MyContract = await ethers.getContractFactory("MyContract");
+    const myContract = await MyContract.deploy(product);
+
+    await myContract.deployed();
+    await myContract.withdraw();
   });
 });
