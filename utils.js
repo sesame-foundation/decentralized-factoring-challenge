c44f09ceb6a8a701a84813aaaba61bedfdf428cc
Max Radin <radin.max@gmail.com>
2022-01-12 23:15:05 -0500

    Updating not equals operator

--- utils.js
+++ utils.js
@@ -4,7 +4,7 @@ function encodeInteger(integer) {
   let value_hex = new bn(integer.toString(), 10).toString("hex");
   return (
     "0x" +
-    (value_hex.length % 64 != 0
+    (value_hex.length % 64 !== 0
       ? "0".repeat(64 - (value_hex.length % 64))
       : "") +
     value_hex
